96f7b142c8b1626f9d4208d0198f9ef3761c65d5
ardittristan <ardittristan@gmail.com>
2020-10-24 03:24:18 +0200

    make code smaller

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 Hooks.on(
   "renderApplication",
-  /**
-   * @param {SettingsConfig} settingsConfig
-   * @param {JQuery} html
-   * @param {Object} data
-   */
-  (settingsConfig, html, data) => {
+  /**@param {SettingsConfig} settingsConfig
+   * @param {JQuery} html */
+  (settingsConfig, html) => {
     if (!(settingsConfig instanceof SettingsConfig)) return;
     html.find("input[data-dtype=Image]").attr("data-dtype", "Img")
   }
