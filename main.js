f414770d855c31ed75260d358dfa0292d6863267
ardittristan <ardittristan@gmail.com>
2020-10-24 03:26:50 +0200

    trigger workflow

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 Hooks.on(
   "renderApplication",
-  /**@param {SettingsConfig} settingsConfig
-   * @param {JQuery} html */
+  /**
+   * @param {SettingsConfig} settingsConfig
+   * @param {JQuery} html
+   * */
   (settingsConfig, html) => {
     if (!(settingsConfig instanceof SettingsConfig)) return;
     html.find("input[data-dtype=Image]").attr("data-dtype", "Img")
